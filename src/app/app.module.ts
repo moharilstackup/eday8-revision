53dc475bff4538ea69fe0450e38b21b3ea69c276
moharilstackup <moharilstackup@gmail.com>
2018-09-06 10:12:09 +0800

    create characters and films components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CharacterListComponent } from './components/character-list.component';
+import { CharacterDetailComponent } from './components/character-detail.component';
+import { FilmListComponent } from './components/film-list.component';
+import { FilmDetailComponent } from './components/film-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterListComponent,
+    CharacterDetailComponent,
+    FilmListComponent,
+    FilmDetailComponent
   ],
   imports: [
     BrowserModule,
